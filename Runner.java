e2633735200fbed297f4f82bd70bb6458de10779
compsci <compsci@compsci>
2017-10-18 09:45:18 -0600

    integer division example, modulus examples

---
+++ Runner.java
@@ -0,0 +1,14 @@
+public class Runner {
+	
+	public static void main(String[] args) {
+	/*** Testing integer division -- truncates! doesnt round off ***/
+	/*** % modulus returns the remainder of the division ***/
+	int numberBits = 10;
+	int bite = 8;
+	int nibble = 4;
+	//test out 10/8 want 1 remainder 2
+	byteAnswer = numberBits / 8;
+	byteRemainder = numberBits %bite;
+		System.out.println("Bits = " + numberbits +"bytes = "+ byteAnswer +"remainder =" byteRemainder);
+	}
+}
